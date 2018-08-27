04d799d4fc114b19aad0af45f175535a81519172
mtgervasoni <michaelgervasoni@gmail.com>
2018-08-26 20:21:16 -0700

    created list of heros (mock an api call) and styled

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombaso'},
+    {id: 14, name: 'Ultimate Warrior'},
+    {id: 15, name: 'The Incredible Hulk'},
+    {id: 16, name: 'Rick Sanchez'},
+    {id: 17, name: 'Fred Savage'},
+    {id: 18, name: 'Limbo Man'},
+
+]
